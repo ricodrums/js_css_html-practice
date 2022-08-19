481337e76352246e95b9a15c40651b9e02415a69
Jessica Berrío <jessica.berrio2104@gmail.com>
2022-08-19 13:23:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/*const principalTitle2 = document.getElementById('title'); es igual que el query
+const principalTitle= document.querySelector('h1');
+
+
+
+//Recibe evento y funcion (este evento permite que cuando se cargue completamente el contenido de
+//la pagina se ejecute lo que hay dentro de este evento)
+window.addEventListener('DOMContentLoaded',()=>{
+  principalTitle.style.color = 'red';
+  //El camel case ya que no se pueden colocar guiones para manipular el CSS
+  principalTitle.style.borderRadius = '15px';
+  principalTitle.style.textShadow = '0 0 10px green';
+})*/
